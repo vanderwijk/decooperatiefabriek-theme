02b16d79ba14cfa6f264e468bd20f8189af2838c
Johan van der Wijk <vanderwijk@users.noreply.github.com>
2020-03-31 14:16:14 +0200

    Punt verwijderd.

--- template-home.php
+++ template-home.php
@@ -28,7 +28,7 @@
 <section class="call-to-action">
 	<div class="container-12">
 		<div class="grid-12">
-			<a href="/programma/rabo-online-masterclasses-risicomanagement/">Hulp bij de Coronacrisis. Rabo online masterClasses Risicomanagement.</a>
+			<a href="/programma/rabo-online-masterclasses-risicomanagement/">Hulp bij de Coronacrisis. Rabo online masterClasses Risicomanagement</a>
 		</div>
 	</div>
 </section>
